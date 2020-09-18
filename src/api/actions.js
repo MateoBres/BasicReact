51b310cf143e255f20bf6e03743ab69d0603944a
Matteo Bresciani <matteo.bresciani@gmail.com>
2020-09-18 13:21:24 -0300

    problemas con condicional en actions

--- src/api/actions.js
+++ src/api/actions.js
@@ -1,4 +1,4 @@
-import form, usuarios from 'reducer'
+
 
 export let aumentarContador = () =>{
     return {type:"CONTADOR_SUMAR"}
@@ -12,19 +12,19 @@ export let resetearContador = () =>{
     return {type:"CONTADOR_RESETEAR"}
 }
 
-export let manejarElSubmit = (e) =>{
+export let manejarElSubmit = (e, store) =>{
     e.preventDefault()
-  if(init.form.editar>=0){
-    let start = init.usuarios.slice(0, init.form.editar)
-    let end = init.usuarios.slice(init.form.editar+1)
-    let usuarioModificado = init.form
+  if(store.form.editar>=0){
+    let start = store.usuarios.slice(0, store.form.editar)
+    let end = store.usuarios.slice(store.form.editar+1)
+    let usuarioModificado = store.form
     let nuevos_usuarios = [
       ...start,
       usuarioModificado,
       ...end
     ]
     return {type: "MANEJAR_USUARIO", nuevos_usuarios}}
-  if (init.form.editar = -1){ return {type: "MANEJAR_SUBMIT"}} }
+  if (store.form.editar = -1){ return {type: "MANEJAR_SUBMIT"}} }
 
 export let manejarCambioNombre = (e) =>{
     let nombre = e.target.value
